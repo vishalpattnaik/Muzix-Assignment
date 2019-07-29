9968854d5477df11e09db586c314e82f636e8215
Vishal Pattnaik <vishalpattnaik387@gmail.com>
2019-07-29 15:29:29 +0530

    removed unused code

--- src/main/java/com/stackroute/domain/Track.java
+++ src/main/java/com/stackroute/domain/Track.java
@@ -23,37 +23,4 @@ public class Track {
     String name;
     String comment;
 
-
-
-   /* public Track(int id, String name, String comment) {
-        this.id = id;
-        this.name = name;
-        this.comment = comment;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-*/
-
 }
